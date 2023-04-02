3db61e8f3c5538cfc9c8782c2c6cb9fdd5437de3
Leon Fortgens <leonfortgens@hotmail.se>
2023-04-02 20:43:05 +0200

    Added Loading skeletons

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,5 +4,5 @@ const getCroppedImageUrl = (url: string) => {
    const index = url.indexOf(target) + target.length
    return url.slice(0, index) + "crop/600/400/"+ url.slice(index)
 }
-
+ 
 export default getCroppedImageUrl
