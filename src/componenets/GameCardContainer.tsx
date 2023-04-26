170a453cf42bc94a9026e2720cc788777ab84a77
Leon Fortgens <leonfortgens@hotmail.se>
2023-04-26 20:17:51 +0200

    Fetch Game Details

--- src/componenets/GameCardContainer.tsx
+++ src/componenets/GameCardContainer.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { easeIn } from "framer-motion";
 import { ReactNode } from "react";
 
 interface Props {
@@ -7,7 +8,15 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="100%" borderRadius={10} overflow="hidden">
+    <Box
+      width="100%"
+      borderRadius={10}
+      overflow="hidden"
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+    >
       {children}
     </Box>
   );
