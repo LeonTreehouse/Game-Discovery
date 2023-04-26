11d63b1d00fdd43ac7e7387c4962801bc74b17b3
Leon Fortgens <leonfortgens@hotmail.se>
2023-04-26 22:13:59 +0200

    Build Game Screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../componenets/ExpandableText";
 import GameAttributes from "../componenets/GameAttributes";
+import GameScreenshots from "../componenets/GameScreenshots";
 import GameTrailer from "../componenets/GameTrailer";
 import useGame from "../hooks/useGame";
 
@@ -14,16 +15,13 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
-      <GridItem>
-        <Heading>{game.name}</Heading>
-        <ExpandableText>{game.description_raw}</ExpandableText>
-        <GameAttributes game={game} />
-      </GridItem>
-      <GridItem>
-        <GameTrailer gameId={game.id} />
-      </GridItem>
-    </SimpleGrid>
+    <>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
+      {/* <GameTrailer gameId={game.id} /> */}
+      <GameScreenshots gameId={game.id} />
+    </>
   );
 };
 
